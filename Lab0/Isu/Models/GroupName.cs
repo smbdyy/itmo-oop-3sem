59168e17f4febe60d99bb49c946742c8d1e1fc1f
Ilya <ividivi67@gmail.com>
2022-09-11 19:17:05 +0300

    feat: GroupName implementation (without exceptions yet)

--- Lab0/Isu/Models/GroupName.cs
+++ Lab0/Isu/Models/GroupName.cs
@@ -1,3 +1,25 @@
 namespace Isu.Models;
 
-public class GroupName { }
+public class GroupName
+{
+    public GroupName(char courseId, CourseNumber courseNumber, int groupNumber)
+    {
+        if (courseId is < 'A' or > 'Z')
+        {
+            // throw
+        }
+
+        if (groupNumber is < 0 or > 99)
+        {
+            // throw
+        }
+
+        this.CourseId = courseId;
+        this.CourseNum = courseNumber;
+        this.GroupNumber = groupNumber;
+    }
+
+    private char CourseId { get; }
+    private CourseNumber CourseNum { get; }
+    private int GroupNumber { get; }
+}
