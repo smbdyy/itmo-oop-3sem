6c9525ee763397a5140ca35698f99f924a7a7900
Ilya <ividivi67@gmail.com>
2022-09-11 17:57:16 +0300

    feat: CourseNumber implementation

--- Lab0/Isu/Models/CourseNumber.cs
+++ Lab0/Isu/Models/CourseNumber.cs
@@ -1,3 +1,29 @@
 namespace Isu.Models;
 
-public class CourseNumber { }
+public enum AcademicDegree
+{
+    Bachelor,
+    Master,
+    Doctor,
+}
+
+public class CourseNumber
+{
+    public CourseNumber(AcademicDegree degree, int number)
+    {
+        if (number is < 1 or > 4)
+        {
+            // throw
+        }
+        else if (degree == AcademicDegree.Master && number > 2)
+        {
+            // throw
+        }
+
+        this.Degree = degree;
+        this.Number = number;
+    }
+
+    private AcademicDegree Degree { get; }
+    private int Number { get; }
+}
