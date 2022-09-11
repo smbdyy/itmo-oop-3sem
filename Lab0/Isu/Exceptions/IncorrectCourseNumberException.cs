b1f82421d44f63b94f737c975c36d2ba7cf77dca
Ilya <ividivi67@gmail.com>
2022-09-11 18:31:07 +0300

    feat: IncorrectCourseNumberException added

---
+++ Lab0/Isu/Exceptions/IncorrectCourseNumberException.cs
@@ -0,0 +1,26 @@
+﻿using Isu.Models;
+
+namespace Isu.Exceptions;
+
+public class IncorrectCourseNumberException : Exception
+{
+    public IncorrectCourseNumberException() { }
+
+    public IncorrectCourseNumberException(AcademicDegree degree, int number)
+    {
+        if (number < 1)
+        {
+            this.Message = "course number cannot be less than 1";
+        }
+
+        this.Message = degree switch
+        {
+            AcademicDegree.Bachelor => "bachelor's max course number is 4",
+            AcademicDegree.Doctor => "doctor's max course number is 4",
+            AcademicDegree.Master => "master's max course number is 2",
+            _ => string.Empty,
+        };
+    }
+
+    public override string Message { get; } = string.Empty;
+}
