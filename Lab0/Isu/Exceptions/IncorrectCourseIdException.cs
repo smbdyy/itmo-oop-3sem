6ea98b4f668c328ed43ddf506c50f3c68eaba323
Ilya <ividivi67@gmail.com>
2022-09-11 19:29:23 +0300

    fix: redundant if in IncorrectCourseIdException feat: IncorrectGroupNumberException added

--- Lab0/Isu/Exceptions/IncorrectCourseIdException.cs
+++ Lab0/Isu/Exceptions/IncorrectCourseIdException.cs
@@ -6,10 +6,7 @@ public class IncorrectCourseIdException : Exception
 
     public IncorrectCourseIdException(char courseId)
     {
-        if (courseId is > 'a' and < 'z')
-        {
-            this.Message = $"course id must be a capital letter ({courseId} is given)";
-        }
+        this.Message = $"course id must be a capital letter ({courseId} is given)";
     }
 
     public override string Message { get; } = "course id must be a capital latin letter";
