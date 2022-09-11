5e84509aa82928dc627db94bd08445275e9d89c0
Ilya <ividivi67@gmail.com>
2022-09-11 19:23:19 +0300

    fix: number info in IncorrectCourseNumberException feat: IncorrectCourseIdException added

---
+++ Lab0/Isu/Exceptions/IncorrectCourseIdException.cs
@@ -0,0 +1,16 @@
+﻿namespace Isu.Exceptions;
+
+public class IncorrectCourseIdException : Exception
+{
+    public IncorrectCourseIdException() { }
+
+    public IncorrectCourseIdException(char courseId)
+    {
+        if (courseId is > 'a' and < 'z')
+        {
+            this.Message = $"course id must be a capital letter ({courseId} is given)";
+        }
+    }
+
+    public override string Message { get; } = "course id must be a capital latin letter";
+}
