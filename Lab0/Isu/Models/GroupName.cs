191987c9657f00d6ca8dd8292be523fe14ee90c1
Ilya <ividivi67@gmail.com>
2022-09-12 12:09:53 +0300

    refactor: one more comment for GroupName.cs

--- Lab0/Isu/Models/GroupName.cs
+++ Lab0/Isu/Models/GroupName.cs
@@ -7,6 +7,7 @@ public class GroupName
     // GroupName consists of specialty identifier (one capital latin letter),
     // course number (1-4 for bachelor or doctor degree, 1-2 for master degree)
     // and group number (0-99, two digits, eg. second group is 02)
+    // therefore, btw, there can only be not more than 100 groups for each specialty and course number
     public GroupName(char specialtyId, CourseNumber courseNumber, int groupNumber)
     {
         if (specialtyId is < 'A' or > 'Z')
