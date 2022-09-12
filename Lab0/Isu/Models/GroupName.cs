883dadc3075afbb5992b38c15c979f7f15a58da1
Ilya <ividivi67@gmail.com>
2022-09-12 12:06:35 +0300

    refactor: GroupName.cs comments

--- Lab0/Isu/Models/GroupName.cs
+++ Lab0/Isu/Models/GroupName.cs
@@ -4,6 +4,9 @@ namespace Isu.Models;
 
 public class GroupName
 {
+    // GroupName consists of specialty identifier (one capital latin letter),
+    // course number (1-4 for bachelor or doctor degree, 1-2 for master degree)
+    // and group number (0-99, two digits, eg. second group is 02)
     public GroupName(char specialtyId, CourseNumber courseNumber, int groupNumber)
     {
         if (specialtyId is < 'A' or > 'Z')
