e8e9474fa844c74c7118b2e4ac0f5beedbfbfabe
Ilya <ividivi67@gmail.com>
2022-09-12 12:10:10 +0300

    refactor: comments for CourseNumber.cs

--- Lab0/Isu/Models/CourseNumber.cs
+++ Lab0/Isu/Models/CourseNumber.cs
@@ -4,6 +4,7 @@ namespace Isu.Models;
 
 public class CourseNumber
 {
+    // there are 3 academic degrees: bachelor (4 courses), master (2 courses) and doctor (4 courses)
     public CourseNumber(AcademicDegree degree, int number)
     {
         if ((number is < 1 or > 4) || (degree == AcademicDegree.Master && number > 2))
