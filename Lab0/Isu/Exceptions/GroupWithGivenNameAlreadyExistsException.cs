a69492525186de522a1e91cba81c7780f7b5b370
Ilya <ividivi67@gmail.com>
2022-09-13 11:04:58 +0300

    feat: GroupWithGivenNameAlreadyExistsException.cs added

---
+++ Lab0/Isu/Exceptions/GroupWithGivenNameAlreadyExistsException.cs
@@ -0,0 +1,15 @@
+﻿using Isu.Models;
+
+namespace Isu.Exceptions;
+
+public class GroupWithGivenNameAlreadyExistsException : Exception
+{
+    public GroupWithGivenNameAlreadyExistsException() { }
+
+    public GroupWithGivenNameAlreadyExistsException(GroupName name)
+    {
+        Message = "group with name {name} already exists";
+    }
+
+    public override string Message { get; } = "group with given name already exists";
+}
