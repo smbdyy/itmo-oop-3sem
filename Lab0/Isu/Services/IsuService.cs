44d96020a32612bce78f1c361705f835697cf51e
Ilya <ividivi67@gmail.com>
2022-09-13 11:04:43 +0300

    feat: IsuService AddGroup method implemented

---
+++ Lab0/Isu/Services/IsuService.cs
@@ -0,0 +1,25 @@
+﻿using Isu.Entities;
+using Isu.Exceptions;
+using Isu.Models;
+
+namespace Isu.Services;
+
+public class IsuService : IIsuService
+{
+    private List<Group> _groups = new ();
+    private List<Student> _students = new ();
+
+    public Group AddGroup(GroupName name)
+    {
+        bool groupWithGivenNameExists = _groups.Any(group => group.Name == name);
+        if (groupWithGivenNameExists)
+        {
+            throw new GroupWithGivenNameAlreadyExistsException(name);
+        }
+
+        var newGroup = new Group(name);
+        _groups.Add(newGroup);
+        return newGroup;
+    }
+
+}
