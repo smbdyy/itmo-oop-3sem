1fe8107ad765ccdc639736b4a7f7b87b4200227a
Ilya <ividivi67@gmail.com>
2022-09-16 17:28:46 +0300

    feat: deleted useless ctor with studentsListSize argument

--- Lab0/Isu/Exceptions/MaxStudentsAmountExceededException.cs
+++ Lab0/Isu/Exceptions/MaxStudentsAmountExceededException.cs
@@ -8,13 +8,7 @@ public class MaxStudentsAmountExceededException : Exception
 
     public MaxStudentsAmountExceededException(Group group)
     {
-        Message = $"max students amount ({group.MaxStudentsAmount} exceeded in group {group.Name.AsString()}";
-    }
-
-    public MaxStudentsAmountExceededException(Group group, int studentsListSize)
-    {
-        Message =
-            $"max students amount ({group.MaxStudentsAmount}) exceeded in group {group.Name.AsString()} ({studentsListSize} students given)";
+        Message = $"max students amount ({group.MaxStudentsAmount}) exceeded in group {group.Name.AsString()}";
     }
 
     public override string Message { get; } = "max students amount for group exceeded";
