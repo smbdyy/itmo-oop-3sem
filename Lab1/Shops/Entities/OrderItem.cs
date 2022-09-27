799923fc60748e9596ecbcd7a7c9b4203e4031ab
Ilya <ividivi67@gmail.com>
2022-09-28 02:23:32 +0300

    feat: class OrderItem

---
+++ Lab1/Shops/Entities/OrderItem.cs
@@ -0,0 +1,21 @@
+﻿using Shops.Exceptions;
+
+namespace Shops.Entities;
+
+public class OrderItem
+{
+    public OrderItem(Product product, int amount = 1)
+    {
+        Product = product;
+
+        if (amount <= 0)
+        {
+            throw new IncorrectOrderProductAmountException(amount);
+        }
+
+        Amount = amount;
+    }
+
+    public Product Product { get; }
+    public int Amount { get; }
+}
---
+++ Lab1/Shops/Exceptions/IncorrectOrderProductAmountException.cs
@@ -0,0 +1,13 @@
+﻿namespace Shops.Exceptions;
+
+public class IncorrectOrderProductAmountException : Exception
+{
+    public IncorrectOrderProductAmountException() { }
+
+    public IncorrectOrderProductAmountException(int amount)
+    {
+        Message = $"cannot order {amount} pcs of a product";
+    }
+
+    public override string Message { get; } = "minimal amount of a product in order is 1";
+}
