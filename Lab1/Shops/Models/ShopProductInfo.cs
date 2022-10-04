deddbb80f0500cdf6a38f84858a17b50c743b188
Ilya <ividivi67@gmail.com>
2022-10-04 14:56:37 +0300

    class ShopProductInfo

---
+++ Lab1/Shops/Models/ShopProductInfo.cs
@@ -0,0 +1,25 @@
+﻿using Shops.Exceptions;
+
+namespace Shops.Models;
+
+public class ShopProductInfo
+{
+    public ShopProductInfo(int amount, decimal price)
+    {
+        if (amount < 0)
+        {
+            throw IncorrectArgumentException.IncorrectProductAmount(amount);
+        }
+
+        if (price <= 0)
+        {
+            throw IncorrectArgumentException.IncorrectPrice(price);
+        }
+
+        Amount = amount;
+        Price = price;
+    }
+
+    public int Amount { get; }
+    public decimal Price { get; }
+}
