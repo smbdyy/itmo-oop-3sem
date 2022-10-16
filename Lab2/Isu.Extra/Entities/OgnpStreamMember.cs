5aa4a58e60ee3fde5e5b80f26648135bc62ef498
Ilya <ividivi67@gmail.com>
2022-10-17 02:17:59 +0300

    feat: add decorator class OgnpStreamMember

---
+++ Lab2/Isu.Extra/Entities/OgnpStreamMember.cs
@@ -0,0 +1,15 @@
+﻿using Isu.Entities;
+
+namespace Isu.Extra.Entities;
+
+public class OgnpStreamMember
+{
+    public OgnpStreamMember(Student student, OgnpStream stream)
+    {
+        Student = student;
+        Stream = stream;
+    }
+
+    public Student Student { get; }
+    public OgnpStream Stream { get; }
+}
