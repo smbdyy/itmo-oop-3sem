5489ae1ab1a018bbd7dc4b98706e9ca1a4f7a5db
Ilya <ividivi67@gmail.com>
2022-10-16 16:15:30 +0300

    feat: OgnpCourse model

---
+++ Lab2/Isu.Extra/Models/OgnpCourse.cs
@@ -0,0 +1,20 @@
+﻿namespace Isu.Extra.Models;
+
+public class OgnpCourse
+{
+    public OgnpCourse(string name, Megafaculty megafaculty)
+    {
+        if (name == string.Empty)
+        {
+            throw new NotImplementedException();
+        }
+
+        Name = name;
+        Megafaculty = megafaculty;
+        Id = Guid.NewGuid();
+    }
+
+    public Guid Id { get; }
+    public string Name { get; }
+    public Megafaculty Megafaculty { get; }
+}
