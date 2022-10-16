451a3045db2ee5bf9172b180431edf27cba726c2
Ilya <ividivi67@gmail.com>
2022-10-16 23:41:50 +0300

    feat: non-static attributes, add constructor

--- Lab2/Isu.Extra/Services/IsuExtraService.cs
+++ Lab2/Isu.Extra/Services/IsuExtraService.cs
@@ -6,12 +6,36 @@ namespace Isu.Extra.Services;
 
 public class IsuExtraService
 {
-    public static TimeOnly LessonsStartTime { get; } = new TimeOnly(8, 20);
-    public static TimeOnly LessonsMaxEndTime { get; } = new TimeOnly(22, 0);
-    public static TimeSpan LessonDuration { get; } = new TimeSpan(1, 30, 0);
-    public static TimeSpan BreakDuration { get; } = new TimeSpan(0, 10, 10);
+    public IsuExtraService(TimeOnly lessonsStartTime, TimeOnly lessonsMaxEndTime, TimeSpan lessonDuration, TimeSpan breakDuration)
+    {
+        if (lessonsStartTime > lessonsMaxEndTime)
+        {
+            throw new NotImplementedException();
+        }
 
-    public static int MaxLessonsAmount { get; } = CountMaxLessonsAmount();
+        if (lessonsStartTime.Add(lessonDuration) > lessonsMaxEndTime)
+        {
+            throw new NotImplementedException();
+        }
+
+        LessonsStartTime = lessonsStartTime;
+        LessonsMaxEndTime = lessonsMaxEndTime;
+        LessonDuration = lessonDuration;
+        BreakDuration = breakDuration;
+
+        MaxLessonsAmount = Convert.ToInt32(Math.Truncate((LessonsMaxEndTime - LessonsStartTime) / (LessonDuration + BreakDuration)));
+        if (LessonsStartTime.Add(((LessonDuration + BreakDuration) * MaxLessonsAmount) + LessonDuration) <= LessonsMaxEndTime)
+        {
+            MaxLessonsAmount++;
+        }
+    }
+
+    public TimeOnly LessonsStartTime { get; }
+    public TimeOnly LessonsMaxEndTime { get; }
+    public TimeSpan LessonDuration { get; }
+    public TimeSpan BreakDuration { get; }
+
+    public int MaxLessonsAmount { get; }
 
     public static Megafaculty GetMegafaculty(GroupName group)
     {
@@ -29,29 +53,18 @@ public class IsuExtraService
         };
     }
 
-    public static TimeOnly GetLessonStartTime(int number)
+    public TimeOnly GetLessonStartTime(int number)
     {
         if (number < 1)
         {
             throw new NotImplementedException();
         }
 
-        if (number < CountMaxLessonsAmount())
+        if (number < MaxLessonsAmount)
         {
             throw new NotImplementedException();
         }
 
         return number == 1 ? LessonsStartTime : LessonsStartTime.Add((LessonDuration + BreakDuration) * number);
-    }
-
-    private static int CountMaxLessonsAmount()
-    {
-        int count = Convert.ToInt32(Math.Truncate((LessonsMaxEndTime - LessonsStartTime) / (LessonDuration + BreakDuration)));
-        if (LessonsStartTime.Add(((LessonDuration + BreakDuration) * count) + LessonDuration) <= LessonsMaxEndTime)
-        {
-            count++;
-        }
-
-        return count;
     }
 }
