0c43fc3344d9389308a7b70ac96d4f12a487f0a6
Ilya <ividivi67@gmail.com>
2022-10-16 23:41:13 +0300

    feat: add subjectName, replaced PersonName with Teacher class

---
+++ Lab2/Isu.Extra/Entities/Lesson.cs
@@ -0,0 +1,34 @@
+﻿using Isu.Extra.Models;
+using Isu.Models;
+
+namespace Isu.Extra.Entities;
+
+public class Lesson
+{
+    public Lesson(Teacher teacher, int timeId, DayOfWeek week, Classroom classroom, string subjectName)
+    {
+        if (timeId < 1)
+        {
+            throw new NotImplementedException();
+        }
+
+        if (subjectName == string.Empty)
+        {
+            throw new NotImplementedException();
+        }
+
+        Teacher = teacher;
+        TimeId = timeId;
+        Week = week;
+        Classroom = classroom;
+        SubjectName = subjectName;
+        Id = Guid.NewGuid();
+    }
+
+    public Teacher Teacher { get; }
+    public int TimeId { get; }
+    public DayOfWeek Week { get; }
+    public Classroom Classroom { get; }
+    public string SubjectName { get; }
+    public Guid Id { get; }
+}
