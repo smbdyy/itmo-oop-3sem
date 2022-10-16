fa9df05cdbcdd218555d6e58bb7a3ea0ee330bfe
Ilya <ividivi67@gmail.com>
2022-10-16 23:40:29 +0300

    feat: add classes OgnpStream and Teacher

---
+++ Lab2/Isu.Extra/Entities/OgnpStream.cs
@@ -0,0 +1,23 @@
+﻿using Isu.Entities;
+using Isu.Extra.Models;
+
+namespace Isu.Extra.Entities;
+
+public class OgnpStream
+{
+    public OgnpStream(OgnpCourse course, int maxMembers)
+    {
+        Course = course;
+        Id = Guid.NewGuid();
+        if (maxMembers < 1)
+        {
+            throw new NotImplementedException();
+        }
+
+        MaxMembers = maxMembers;
+    }
+
+    public OgnpCourse Course { get; }
+    public Guid Id { get; }
+    public int MaxMembers { get; }
+}
---
+++ Lab2/Isu.Extra/Entities/Teacher.cs
@@ -0,0 +1,15 @@
+﻿using Isu.Models;
+
+namespace Isu.Extra.Entities;
+
+public class Teacher
+{
+    public Teacher(PersonName name)
+    {
+        Name = name;
+        Id = Guid.NewGuid();
+    }
+
+    public PersonName Name { get; }
+    public Guid Id { get; }
+}
