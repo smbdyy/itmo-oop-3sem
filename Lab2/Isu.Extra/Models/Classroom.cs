03270c4598d315535d0de349342f05ad8ffdaefe
Ilya <ividivi67@gmail.com>
2022-10-16 17:23:02 +0300

    feat: add class Classroom and class Lesson

---
+++ Lab2/Isu.Extra/Models/Classroom.cs
@@ -0,0 +1,26 @@
+﻿namespace Isu.Extra.Models;
+
+public struct Classroom
+{
+    public Classroom(int block, int floor, int number)
+    {
+        if (block is < 1 or > 9)
+        {
+            throw new NotImplementedException();
+        }
+
+        if (floor is < 1 or > 9)
+        {
+            throw new NotImplementedException();
+        }
+
+        if (number is < 1 or > 99)
+        {
+            throw new NotImplementedException();
+        }
+
+        Name = (block * 1000) + (floor * 100) + number;
+    }
+
+    public int Name { get; }
+}
---
+++ Lab2/Isu.Extra/Models/Lesson.cs
@@ -0,0 +1,24 @@
+﻿using Isu.Models;
+
+namespace Isu.Extra.Models;
+
+public class Lesson
+{
+    public Lesson(PersonName teacher, int timeId, DayOfWeek week, Classroom classroom)
+    {
+        if (timeId < 1)
+        {
+            throw new NotImplementedException();
+        }
+
+        Teacher = teacher;
+        TimeId = timeId;
+        Week = week;
+        Classroom = classroom;
+    }
+
+    public PersonName Teacher { get; }
+    public int TimeId { get; }
+    public DayOfWeek Week { get; }
+    public Classroom Classroom { get; }
+}
