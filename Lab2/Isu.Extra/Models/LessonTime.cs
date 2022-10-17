c0efb0616537c902e485b44274e9ac9eaf28a401
Ilya <ividivi67@gmail.com>
2022-10-17 13:47:12 +0300

    feat: operator ==

--- Lab2/Isu.Extra/Models/LessonTime.cs
+++ Lab2/Isu.Extra/Models/LessonTime.cs
@@ -17,4 +17,29 @@ public class LessonTime
 
     public int TimeId { get; }
     public DayOfWeek Week { get; }
+
+    public static bool operator ==(LessonTime a, LessonTime b)
+    {
+        return a.TimeId == b.TimeId && a.Week == b.Week;
+    }
+
+    public static bool operator !=(LessonTime a, LessonTime b)
+    {
+        return !(a == b);
+    }
+
+    public override bool Equals(object? o)
+    {
+        if (o is null)
+        {
+            return false;
+        }
+
+        return o.GetType() == GetType() && this == (LessonTime)o;
+    }
+
+    public override int GetHashCode()
+    {
+        return (int)(TimeId * 10 + Week);
+    }
 }
