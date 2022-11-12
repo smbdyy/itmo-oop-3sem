c2dd239abc367859e7326d3e7994c72dfce29770
Ilya <ividivi67@gmail.com>
2022-11-12 05:43:57 +0300

    feat: add property Name

--- Lab3/Backups/Models/Storage.cs
+++ Lab3/Backups/Models/Storage.cs
@@ -1,13 +1,22 @@
-﻿namespace Backups.Models;
+﻿using Zio;
+
+namespace Backups.Models;
 
 public class Storage
 {
     private readonly List<BackupObject> _backupObjects;
 
-    public Storage(IEnumerable<BackupObject> backupObjects)
+    public Storage(string name, IEnumerable<BackupObject> backupObjects)
     {
+        if (name == string.Empty || name.Contains('/') || name.Contains('\\') || name.Contains(':'))
+        {
+            throw new NotImplementedException();
+        }
+
+        Name = name;
         _backupObjects = new List<BackupObject>(backupObjects);
     }
 
     public IReadOnlyList<BackupObject> BackupObjects => _backupObjects;
+    public string Name { get; }
 }
