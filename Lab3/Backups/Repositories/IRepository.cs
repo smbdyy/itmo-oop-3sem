143c95b0c5a7924ecc8ae7e1e1a7f4938920183c
Ilya <ividivi67@gmail.com>
2022-11-14 02:44:56 +0300

    feat: add method CopyDirectoryToDirectory

--- Lab3/Backups/Repositories/IRepository.cs
+++ Lab3/Backups/Repositories/IRepository.cs
@@ -10,4 +10,25 @@ public interface IRepository
     public IFileSystem RepositoryFileSystem { get; }
 
     public void SaveRestorePoint(RestorePoint restorePoint);
+
+    public void CopyDirectoryToDirectory(UPath source, UPath destination)
+    {
+        if (!RepositoryFileSystem.DirectoryExists(source))
+        {
+            throw new NotImplementedException();
+        }
+
+        UPath dirName = Path.GetFileName(source.FullName);
+        RepositoryFileSystem.CreateDirectory(UPath.Combine(destination, dirName));
+
+        foreach (UPath file in RepositoryFileSystem.EnumerateFiles(source))
+        {
+            RepositoryFileSystem.CopyFile(UPath.Combine(source, file), UPath.Combine(destination, file), true);
+        }
+
+        foreach (UPath dir in RepositoryFileSystem.EnumerateFiles(source))
+        {
+            CopyDirectoryToDirectory(UPath.Combine(source, dir), UPath.Combine(destination, dir));
+        }
+    }
 }
