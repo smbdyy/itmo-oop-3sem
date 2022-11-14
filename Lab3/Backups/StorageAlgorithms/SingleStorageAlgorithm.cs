158528816568ae5853b1bc4347803ec3336a2cce
Ilya <ividivi67@gmail.com>
2022-11-14 04:11:49 +0300

    feat: add SingleStorageAlgorithm implementation

---
+++ Lab3/Backups/StorageAlgorithms/SingleStorageAlgorithm.cs
@@ -0,0 +1,13 @@
+﻿using Backups.Models;
+
+namespace Backups.StorageAlgorithms;
+
+public class SingleStorageAlgorithm : IStorageAlgorithm
+{
+    public IEnumerable<Storage> MakeStorages(int id, IEnumerable<BackupObject> backupObjects)
+    {
+        string name = $"Storage_{id}";
+        var storage = new Storage(name, backupObjects);
+        return new List<Storage> { storage };
+    }
+}
