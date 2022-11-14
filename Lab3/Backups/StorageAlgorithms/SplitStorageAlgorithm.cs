cbd48a7ca76fae5500e10f2267c2286eaaef8c9a
Ilya <ividivi67@gmail.com>
2022-11-14 04:18:32 +0300

    feat: add SplitStorageAlgorithm implementation

---
+++ Lab3/Backups/StorageAlgorithms/SplitStorageAlgorithm.cs
@@ -0,0 +1,20 @@
+﻿using Backups.Models;
+
+namespace Backups.StorageAlgorithms;
+
+public class SplitStorageAlgorithm : IStorageAlgorithm
+{
+    public IEnumerable<Storage> MakeStorages(int id, IEnumerable<BackupObject> backupObjects)
+    {
+        var storages = new List<Storage>();
+        foreach (BackupObject backupObject in backupObjects)
+        {
+            string fileName = Path.GetFileName(backupObject.RelativePath.FullName);
+            string storageName = $"{fileName}({id}).zip";
+            var storage = new Storage(storageName, new List<BackupObject> { backupObject });
+            storages.Add(storage);
+        }
+
+        return storages;
+    }
+}
