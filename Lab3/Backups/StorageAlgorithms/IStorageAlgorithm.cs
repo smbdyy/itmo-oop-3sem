6d766d19af89d7dbd098bcf0bdf29556bf5c0eed
Ilya <ividivi67@gmail.com>
2022-11-23 22:03:12 +0300

    clear

--- Lab3/Backups/StorageAlgorithms/IStorageAlgorithm.cs
+++
@@ -1,8 +0,0 @@
-﻿using Backups.Models;
-
-namespace Backups.StorageAlgorithms;
-
-public interface IStorageAlgorithm
-{
-    public IEnumerable<Storage> MakeStorages(int id, IEnumerable<BackupObject> backupObjects);
-}
