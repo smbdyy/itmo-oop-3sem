61d3494f754eb5290f0d2dc6e828c410ce599e9e
Ilya <ividivi67@gmail.com>
2022-11-25 14:24:43 +0300

    feat: add FileSystemRepositoryFolder implementation

---
+++ Lab3/Backups/Repositories/FileSystemRepositoryFolder.cs
@@ -0,0 +1,18 @@
+﻿namespace Backups.Repositories;
+
+public class FileSystemRepositoryFolder : RepositoryFolder
+{
+    private readonly List<IRepositoryObject> _entries;
+
+    public FileSystemRepositoryFolder()
+    {
+        _entries = new List<IRepositoryObject>();
+    }
+
+    public FileSystemRepositoryFolder(IEnumerable<IRepositoryObject> entries)
+    {
+        _entries = entries.ToList();
+    }
+
+    public override IReadOnlyCollection<IRepositoryObject> Entries => _entries;
+}
