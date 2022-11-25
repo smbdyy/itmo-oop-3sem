db8dbad8c2e994644b5715a0fb5fab59aad87435
Ilya <ividivi67@gmail.com>
2022-11-25 14:18:44 +0300

    feat: add FileSystemRepositoryFile implementation

---
+++ Lab3/Backups/Repositories/FileSystemRepositoryFile.cs
@@ -0,0 +1,16 @@
+﻿namespace Backups.Repositories;
+
+public class FileSystemRepositoryFile : RepositoryFile
+{
+    private string _fullPath;
+
+    public FileSystemRepositoryFile(string fullPath)
+    {
+        _fullPath = fullPath;
+    }
+
+    public override Stream Open()
+    {
+        return File.Open(_fullPath, FileMode.Open, FileAccess.ReadWrite);
+    }
+}
