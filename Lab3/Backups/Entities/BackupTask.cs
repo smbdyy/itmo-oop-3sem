b1bb17fa64f4755a063ab8d53baced6cdc215470
Ilya <ividivi67@gmail.com>
2022-11-25 14:18:20 +0300

    feat: add class BackupTask

---
+++ Lab3/Backups/Entities/BackupTask.cs
@@ -0,0 +1,39 @@
+﻿using Backups.Models;
+using Backups.Repositories;
+using Backups.StorageAlgorithms;
+
+namespace Backups.Entities;
+
+public class BackupTask
+{
+    private readonly List<RestorePoint> _restorePoints = new ();
+    private readonly List<BackupObject> _backupObjects = new ();
+
+    public BackupTask(string name, IRepository repository, IStorageAlgorithm storageAlgorithm)
+    {
+        Name = ValidateName(name);
+        Repository = repository;
+        StorageAlgorithm = storageAlgorithm;
+    }
+
+    public string Name { get; }
+    public IRepository Repository { get; }
+    public IStorageAlgorithm StorageAlgorithm { get; }
+    public IReadOnlyCollection<RestorePoint> RestorePoints => _restorePoints;
+    public IReadOnlyCollection<BackupObject> BackupObjects => _backupObjects;
+
+    private static string ValidateName(string value)
+    {
+        if (value == string.Empty)
+        {
+            throw new NotImplementedException();
+        }
+
+        if (value.IndexOfAny(Path.GetInvalidFileNameChars()) >= 0)
+        {
+            throw new NotImplementedException();
+        }
+
+        return value;
+    }
+}
