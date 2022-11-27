d6b0841721334c70bcf082f0b5849683ce81bb03
Ilya <ividivi67@gmail.com>
2022-11-27 17:30:29 +0300

    feat: add Storage property

--- Lab3/Backups/Models/RestorePoint.cs
+++ Lab3/Backups/Models/RestorePoint.cs
@@ -1,14 +1,18 @@
-﻿namespace Backups.Models;
+﻿using Backups.StorageAlgorithms;
+
+namespace Backups.Models;
 
 public class RestorePoint
 {
     private List<BackupObject> _backupObjects;
 
-    public RestorePoint(IEnumerable<BackupObject> backupObjects)
+    public RestorePoint(IEnumerable<BackupObject> backupObjects, IStorage storage)
     {
         _backupObjects = backupObjects.ToList();
+        Storage = storage;
     }
 
     public IReadOnlyCollection<BackupObject> BackupObjects => _backupObjects;
     public DateTime CreationDateTime { get; } = DateTime.Now;
+    public IStorage Storage { get; }
 }
