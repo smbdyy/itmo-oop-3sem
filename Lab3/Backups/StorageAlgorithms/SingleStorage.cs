c31f85bc10a610d85eac8bfe47f594224fb2a694
Ilya <ividivi67@gmail.com>
2022-11-27 23:13:17 +0300

    feat: add SingleStorage implementation

---
+++ Lab3/Backups/StorageAlgorithms/SingleStorage.cs
@@ -0,0 +1,25 @@
+﻿using Backups.Archivers;
+using Backups.Repositories;
+
+namespace Backups.StorageAlgorithms;
+
+public class SingleStorage : IStorage
+{
+    private List<IStorageArchive> _archives;
+
+    public SingleStorage(IEnumerable<IStorageArchive> archives)
+    {
+        _archives = archives.ToList();
+    }
+
+    public IReadOnlyCollection<IRepositoryObject> GetEntries()
+    {
+        var entries = new List<IRepositoryObject>();
+        foreach (IStorageArchive archive in _archives)
+        {
+            entries.AddRange(archive.GetEntries());
+        }
+
+        return entries;
+    }
+}
