22714e4643d061b03e9baf2e9784657844a04477
Ilya <ividivi67@gmail.com>
2022-11-27 17:31:52 +0300

    fix: path validation from repository feat: add Archive and CreateRestorePoint()

--- Lab3/Backups/Entities/BackupTask.cs
+++ Lab3/Backups/Entities/BackupTask.cs
@@ -1,39 +1,34 @@
-﻿using Backups.Models;
+﻿using Backups.Archivers;
+using Backups.Models;
 using Backups.Repositories;
 using Backups.StorageAlgorithms;
 
 namespace Backups.Entities;
 
+// TODO interface maybe
 public class BackupTask
 {
     private readonly List<RestorePoint> _restorePoints = new ();
     private readonly List<BackupObject> _backupObjects = new ();
 
-    public BackupTask(string name, IRepository repository, IStorageAlgorithm storageAlgorithm)
+    public BackupTask(string name, IRepository repository, IStorageAlgorithm storageAlgorithm, IStorageArchiver archiver)
     {
-        Name = ValidateName(name);
+        Name = repository.ValidateRelativePath(name);
         Repository = repository;
         StorageAlgorithm = storageAlgorithm;
+        Archiver = archiver;
     }
 
     public string Name { get; }
     public IRepository Repository { get; }
     public IStorageAlgorithm StorageAlgorithm { get; }
+    public IStorageArchiver Archiver { get; }
     public IReadOnlyCollection<RestorePoint> RestorePoints => _restorePoints;
     public IReadOnlyCollection<BackupObject> BackupObjects => _backupObjects;
 
-    private static string ValidateName(string value)
+    public void CreateRestorePoint()
     {
-        if (value == string.Empty)
-        {
-            throw new NotImplementedException();
-        }
-
-        if (value.IndexOfAny(Path.GetInvalidFileNameChars()) >= 0)
-        {
-            throw new NotImplementedException();
-        }
-
-        return value;
+        IStorage storage = StorageAlgorithm.MakeStorage(Repository, Archiver, BackupObjects);
+        _restorePoints.Add(new RestorePoint(BackupObjects, storage));
     }
 }
