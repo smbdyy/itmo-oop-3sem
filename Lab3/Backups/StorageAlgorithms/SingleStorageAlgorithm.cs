014227ba5bf925fa771fa7bc4a2653b50008c9d6
Ilya <ividivi67@gmail.com>
2022-11-27 23:31:15 +0300

    feat: add SingleStorageAlgorithm implementation

---
+++ Lab3/Backups/StorageAlgorithms/SingleStorageAlgorithm.cs
@@ -0,0 +1,20 @@
+﻿using Backups.Archivers;
+using Backups.Models;
+using Backups.Repositories;
+
+namespace Backups.StorageAlgorithms;
+
+public class SingleStorageAlgorithm : IStorageAlgorithm
+{
+    public IStorage MakeStorage(int id, IRepository repository, IStorageArchiver storageArchiver, IEnumerable<BackupObject> objects)
+    {
+        var archives = new List<IStorageArchive>();
+        foreach (BackupObject backupObject in objects)
+        {
+            string archiveName = $"{Path.GetFileName(backupObject.Path)}({id})";
+            archives.Add(storageArchiver.CreateArchive(archiveName, repository, backupObject));
+        }
+
+        return new SingleStorage(archives);
+    }
+}
