12bbe9e1e4637eaec975ecbd2ab44bbe3fc456fc
Ilya <ividivi67@gmail.com>
2022-11-27 17:34:23 +0300

    fix: make StorageAlgorithm and Archiver private

--- Lab3/Backups/Entities/BackupTask.cs
+++ Lab3/Backups/Entities/BackupTask.cs
@@ -5,30 +5,29 @@ using Backups.StorageAlgorithms;
 
 namespace Backups.Entities;
 
-// TODO interface maybe
 public class BackupTask
 {
     private readonly List<RestorePoint> _restorePoints = new ();
     private readonly List<BackupObject> _backupObjects = new ();
+    private IStorageAlgorithm _storageAlgorithm;
+    private IStorageArchiver _archiver;
 
     public BackupTask(string name, IRepository repository, IStorageAlgorithm storageAlgorithm, IStorageArchiver archiver)
     {
         Name = repository.ValidateRelativePath(name);
         Repository = repository;
-        StorageAlgorithm = storageAlgorithm;
-        Archiver = archiver;
+        _storageAlgorithm = storageAlgorithm;
+        _archiver = archiver;
     }
 
     public string Name { get; }
     public IRepository Repository { get; }
-    public IStorageAlgorithm StorageAlgorithm { get; }
-    public IStorageArchiver Archiver { get; }
     public IReadOnlyCollection<RestorePoint> RestorePoints => _restorePoints;
     public IReadOnlyCollection<BackupObject> BackupObjects => _backupObjects;
 
     public void CreateRestorePoint()
     {
-        IStorage storage = StorageAlgorithm.MakeStorage(Repository, Archiver, BackupObjects);
+        IStorage storage = _storageAlgorithm.MakeStorage(Repository, _archiver, BackupObjects);
         _restorePoints.Add(new RestorePoint(BackupObjects, storage));
     }
 }
