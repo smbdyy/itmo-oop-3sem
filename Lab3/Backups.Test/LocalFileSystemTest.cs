fd2f221a27b88363018f1d853ebdb7533f4d235e
Ilya <ividivi67@gmail.com>
2022-11-28 21:27:28 +0300

    test: local file system tests

---
+++ Lab3/Backups.Test/LocalFileSystemTest.cs
@@ -0,0 +1,54 @@
+﻿using Backups.Archivers;
+using Backups.Entities;
+using Backups.Models;
+using Backups.Repositories;
+using Backups.StorageAlgorithms;
+using Xunit;
+
+namespace Backups.Test;
+
+public class LocalFileSystemTest
+{
+    [Fact(Skip = "local fs")]
+    public void CreateRestorePoints_SplitStoragesCreated()
+    {
+        const string restorePointsPath = "RestorePoints";
+        var repository = new FileSystemRepository(@"C:\BackupsLab", restorePointsPath);
+        var backupTask = new BackupTask("TestTask", repository, new SplitStorageAlgorithm(), new ZipStorageArchiver());
+        var fileBackupObject = new BackupObject("file1.txt");
+        var dirBackupObject = new BackupObject("dir1");
+        backupTask.AddBackupObject(fileBackupObject);
+        backupTask.AddBackupObject(dirBackupObject);
+        backupTask.CreateRestorePoint();
+
+        Assert.True(backupTask.Repository.FileExists(Path.Combine(restorePointsPath, "file1.txt(1).zip")));
+        Assert.True(backupTask.Repository.FileExists(Path.Combine(restorePointsPath, "dir1(1).zip")));
+        Assert.Equal(2, backupTask.Repository.EnumerateFileSystemEntries(restorePointsPath).Count());
+
+        backupTask.RemoveBackupObject(dirBackupObject);
+        backupTask.CreateRestorePoint();
+
+        Assert.True(backupTask.Repository.FileExists(Path.Combine(restorePointsPath, "file1.txt(2).zip")));
+        Assert.Equal(3, backupTask.Repository.EnumerateFileSystemEntries(restorePointsPath).Count());
+
+        backupTask.Repository.DeleteFile(Path.Combine(restorePointsPath, "file1.txt(1).zip"));
+        backupTask.Repository.DeleteFile(Path.Combine(restorePointsPath, "file1.txt(2).zip"));
+        backupTask.Repository.DeleteFile(Path.Combine(restorePointsPath, "dir1(1).zip"));
+    }
+
+    [Fact(Skip = "local fs")]
+    public void CreateRestorePoint_SingleStorageCreated()
+    {
+        const string restorePointsPath = "RestorePoints";
+        var repository = new FileSystemRepository(@"C:\BackupsLab", restorePointsPath);
+        var backupTask = new BackupTask("TestTask", repository, new SingleStorageAlgorithm(), new ZipStorageArchiver());
+        var fileBackupObject = new BackupObject("file1.txt");
+        var dirBackupObject = new BackupObject("dir1");
+        backupTask.AddBackupObject(fileBackupObject);
+        backupTask.AddBackupObject(dirBackupObject);
+        backupTask.CreateRestorePoint();
+
+        Assert.True(backupTask.Repository.FileExists(Path.Combine(restorePointsPath, "RestorePoint_1.zip")));
+        Assert.Single(backupTask.Repository.EnumerateFileSystemEntries(restorePointsPath));
+    }
+}
