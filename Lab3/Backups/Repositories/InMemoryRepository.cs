6e8dbdfdd4246a008ca6a0f2a0192f046a4e582d
Ilya <ividivi67@gmail.com>
2022-11-30 00:42:25 +0300

    feat: add InMemoryRepository class (not fully implemented)

---
+++ Lab3/Backups/Repositories/InMemoryRepository.cs
@@ -0,0 +1,30 @@
+﻿using Backups.Tools.Exceptions;
+
+namespace Backups.Repositories;
+
+public class InMemoryRepository : IRepository
+{
+    private readonly List<IMemoryFileSystemObject> _rootDirectoryEntries = new ();
+
+    public InMemoryRepository(string restorePointsPath)
+    {
+        RestorePointsPath = ValidateRelativePath(restorePointsPath);
+    }
+
+    public string RestorePointsPath { get; }
+
+    public string ValidateRelativePath(string path)
+    {
+        if (path == string.Empty)
+        {
+            throw BackupsArgumentException.EmptyPathString();
+        }
+
+        if (Path.IsPathRooted(path) || Path.EndsInDirectorySeparator(path) || path.IndexOfAny(Path.GetInvalidPathChars()) >= 0)
+        {
+            throw RepositoryException.IncorrectRelativePath(path);
+        }
+
+        return path;
+    }
+}
