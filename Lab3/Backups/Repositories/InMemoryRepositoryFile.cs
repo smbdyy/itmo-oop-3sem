74c92cc3f35e2d452bae745c459be23a669582c4
Ilya <ividivi67@gmail.com>
2022-11-30 00:41:47 +0300

    feat: add InMemoryRepositoryFile

---
+++ Lab3/Backups/Repositories/InMemoryRepositoryFile.cs
@@ -0,0 +1,17 @@
+﻿using Backups.Visitors;
+
+namespace Backups.Repositories;
+
+public class InMemoryRepositoryFile : IRepositoryFile
+{
+    private readonly IMemoryFileSystemFile _file;
+
+    public InMemoryRepositoryFile(IMemoryFileSystemFile file)
+    {
+        _file = file;
+    }
+
+    public string Path => _file.Path;
+    public void Accept(IRepositoryVisitor visitor) => visitor.Visit(this);
+    public Stream Open() => _file.OpenRead();
+}
