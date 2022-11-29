941161a99cec01d2f94d1e5d4d86298246f49eb3
Ilya <ividivi67@gmail.com>
2022-11-30 00:23:48 +0300

    feat: add MemoryFileSystemFolder

---
+++ Lab3/Backups/Repositories/MemoryFileSystemFolder.cs
@@ -0,0 +1,27 @@
+﻿using Backups.Visitors;
+
+namespace Backups.Repositories;
+
+public class MemoryFileSystemFolder : IMemoryFileSystemFolder
+{
+    private readonly List<IMemoryFileSystemObject> _entries;
+
+    public MemoryFileSystemFolder()
+    {
+        _entries = new List<IMemoryFileSystemObject>();
+    }
+
+    public MemoryFileSystemFolder(IEnumerable<IMemoryFileSystemObject> entries)
+    {
+        _entries = entries.ToList();
+    }
+
+    public IReadOnlyCollection<IMemoryFileSystemObject> Entries => _entries;
+
+    public void Accept(IMemoryFileSystemVisitor visitor) => visitor.Visit(this);
+
+    public void AddEntry(IMemoryFileSystemObject entry)
+    {
+        _entries.Add(entry);
+    }
+}
