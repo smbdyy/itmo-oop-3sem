0acbafc97411f113ab091cfaafb7c92c12397750
Ilya <ividivi67@gmail.com>
2022-11-30 15:28:19 +0300

    feat: add memory file system and repository visitors

---
+++ Lab3/Backups/Visitors/MemoryFolderFinderVisitor.cs
@@ -0,0 +1,33 @@
+﻿using Backups.Repositories;
+
+namespace Backups.Visitors;
+
+public class MemoryFolderFinderVisitor : IMemoryFileSystemVisitor
+{
+    private IMemoryFileSystemFolder? _folder;
+    private string _path;
+
+    public MemoryFolderFinderVisitor(string path)
+    {
+        _path = path;
+    }
+
+    public void Visit(IMemoryFileSystemFile file) { }
+
+    public void Visit(IMemoryFileSystemFolder folder)
+    {
+        if (folder.Path == _path)
+        {
+            _folder = folder;
+        }
+
+        foreach (IMemoryFileSystemObject entry in folder.Entries)
+        {
+            entry.Accept(this);
+            if (_folder is not null)
+            {
+                return;
+            }
+        }
+    }
+}
