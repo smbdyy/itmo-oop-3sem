9e841f81410c9c15dcae8eaeb6624796fb1ac4a8
Ilya <ividivi67@gmail.com>
2022-11-30 15:27:38 +0300

    feat: add InMemoryRepositoryFolder

---
+++ Lab3/Backups/Repositories/InMemoryRepositoryFolder.cs
@@ -0,0 +1,18 @@
+﻿using Backups.Visitors;
+
+namespace Backups.Repositories;
+
+public class InMemoryRepositoryFolder : IRepositoryFolder
+{
+    private List<IRepositoryObject> _entries;
+
+    public InMemoryRepositoryFolder(string path, IEnumerable<IRepositoryObject> entries)
+    {
+        Path = path;
+        _entries = entries.ToList();
+    }
+
+    public string Path { get; }
+    public IReadOnlyCollection<IRepositoryObject> Entries => _entries;
+    public void Accept(IRepositoryVisitor visitor) => visitor.Visit(this);
+}
