15621022191416d7918dd32725297aa678198749
Ilya <ividivi67@gmail.com>
2022-11-30 15:28:34 +0300

    feat: add MemoryFileSystemUtils

---
+++ Lab3/Backups/Tools/Utils/MemoryFileSystemUtils.cs
@@ -0,0 +1,26 @@
+﻿namespace Backups.Tools.Utils;
+
+public static class MemoryFileSystemUtils
+{
+    public static IEnumerable<string> SplitPath(string path)
+    {
+        return path.Split(Path.DirectorySeparatorChar);
+    }
+
+    public static string CombineFromArray(IEnumerable<string> splitPath)
+    {
+        string[] splitPathAsArray = splitPath.ToArray();
+        if (!splitPathAsArray.Any())
+        {
+            throw new NotImplementedException();
+        }
+
+        string result = splitPathAsArray[0];
+        for (int i = 1; i < splitPathAsArray.Length; i++)
+        {
+            result = Path.Combine(result, splitPathAsArray[i]);
+        }
+
+        return result;
+    }
+}
