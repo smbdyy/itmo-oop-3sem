3e42a732df535f907133eccf90650c01a0c6eac5
Ilya <ividivi67@gmail.com>
2022-12-06 20:35:56 +0300

    feat: add PersonName model

---
+++ Lab4/Banks/Models/PersonName.cs
@@ -0,0 +1,28 @@
+﻿namespace Banks.Models;
+
+public class PersonName
+{
+    public PersonName(string name, string surname)
+    {
+        Name = Validate(name);
+        Surname = Validate(surname);
+    }
+
+    public string Name { get; }
+    public string Surname { get; }
+
+    private static string Validate(string value)
+    {
+        if (value == string.Empty || !value.All(IsLatinLetter) || !char.IsUpper(value[0]))
+        {
+            throw new NotImplementedException();
+        }
+
+        return value;
+    }
+
+    private static bool IsLatinLetter(char symbol)
+    {
+        return symbol is >= 'a' and <= 'z' or >= 'A' and <= 'Z';
+    }
+}
