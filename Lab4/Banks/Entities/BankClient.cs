798a925f314af0be560ba40154ab32943342acd8
Ilya <ividivi67@gmail.com>
2022-12-06 20:44:56 +0300

    feat: add BankClient entity

---
+++ Lab4/Banks/Entities/BankClient.cs
@@ -0,0 +1,23 @@
+﻿using Banks.Models;
+
+namespace Banks.Entities;
+
+public class BankClient
+{
+    public BankClient(PersonName name, PassportNumber passportNumber, Address address)
+    {
+        Name = name;
+        PassportNumber = passportNumber;
+        Address = address;
+    }
+
+    public BankClient(PersonName name)
+    {
+        Name = name;
+    }
+
+    public Guid Id { get; } = Guid.NewGuid();
+    public PersonName Name { get; }
+    public PassportNumber? PassportNumber { get; set; }
+    public Address? Address { get; set; }
+}
