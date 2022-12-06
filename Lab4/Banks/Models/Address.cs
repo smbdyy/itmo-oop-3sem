621e47554840837d270ae14cc7532fe648bd486b
Ilya <ividivi67@gmail.com>
2022-12-06 20:25:06 +0300

    feat: add Address and PassportNumber models

---
+++ Lab4/Banks/Models/Address.cs
@@ -0,0 +1,27 @@
+﻿namespace Banks.Models;
+
+public class Address
+{
+    public Address(string country, string town, string street, string houseNumber)
+    {
+        Country = ValidateNotEmpty(country);
+        Town = ValidateNotEmpty(town);
+        Street = ValidateNotEmpty(street);
+        HouseNumber = ValidateNotEmpty(houseNumber);
+    }
+
+    public string Country { get; }
+    public string Town { get; }
+    public string Street { get; }
+    public string HouseNumber { get; }
+
+    public static string ValidateNotEmpty(string value)
+    {
+        if (value == string.Empty)
+        {
+            throw new NotImplementedException();
+        }
+
+        return value;
+    }
+}
---
+++ Lab4/Banks/Models/PassportNumber.cs
@@ -0,0 +1,24 @@
+﻿namespace Banks.Models;
+
+public class PassportNumber
+{
+    public PassportNumber(string series, string number)
+    {
+        if (series.Length != 4 || series.Any(symbol => !char.IsDigit(symbol)) ||
+            number.Length != 6 || number.Any(symbol => !char.IsDigit(symbol)))
+        {
+            throw new NotImplementedException();
+        }
+
+        Series = series;
+        Number = number;
+    }
+
+    public string Series { get; }
+    public string Number { get; }
+
+    public string AsString()
+    {
+        return $"{Series} {Number}";
+    }
+}
