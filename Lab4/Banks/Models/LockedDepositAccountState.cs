defbfe9163619b2a12bbc2459b0a7c9bf458a439
Ilya <ividivi67@gmail.com>
2022-12-09 23:20:16 +0300

    feat: add LockedDepositAccountState

---
+++ Lab4/Banks/Models/LockedDepositAccountState.cs
@@ -0,0 +1,27 @@
+﻿using Banks.Entities;
+using Banks.Interfaces;
+
+namespace Banks.Models;
+
+public class LockedDepositAccountState : IAccountState
+{
+    public decimal Withdraw(decimal accountMoney, decimal amount)
+    {
+        throw new NotImplementedException();
+    }
+
+    public decimal Replenish(decimal accountMoney, decimal amount)
+    {
+        if (amount < 0)
+        {
+            throw new NotImplementedException();
+        }
+
+        return accountMoney + amount;
+    }
+
+    public decimal Send(TransferTransaction transaction)
+    {
+        throw new NotImplementedException();
+    }
+}
