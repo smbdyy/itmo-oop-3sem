02468a5b65f1192385dacd8fa87cc7ac9b93bc83
Ilya <ividivi67@gmail.com>
2022-12-09 19:58:16 +0300

    feat: add bank and bank account interfaces

---
+++ Lab4/Banks/Interfaces/IBank.cs
@@ -0,0 +1,11 @@
+﻿using Banks.Entities;
+
+namespace Banks.Interfaces;
+
+public interface IBank
+{
+    public Guid Id { get; }
+    public string Name { get; }
+    public IReadOnlyCollection<IBankAccount> Accounts { get; }
+    public IReadOnlyCollection<BankClient> Clients { get; }
+}
---
+++ Lab4/Banks/Interfaces/IBankAccount.cs
@@ -0,0 +1,12 @@
+﻿using Banks.Entities;
+
+namespace Banks.Interfaces;
+
+public interface IBankAccount
+{
+    public BankClient Client { get; }
+    public decimal MoneyAmount { get; }
+    public decimal Withdraw(decimal amount);
+    public void Replenish(decimal amount);
+    public void Transfer(decimal amount, IBankAccount recipient);
+}
