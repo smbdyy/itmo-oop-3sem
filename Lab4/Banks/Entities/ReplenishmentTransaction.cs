7461b3de6aa9d7fb57bd61920d7157a4ef31712e
Ilya <ividivi67@gmail.com>
2022-12-09 20:07:02 +0300

    feat: add transaction interface and implementations

---
+++ Lab4/Banks/Entities/ReplenishmentTransaction.cs
@@ -0,0 +1,26 @@
+﻿using Banks.Interfaces;
+
+namespace Banks.Entities;
+
+public class ReplenishmentTransaction : ITransaction
+{
+    public ReplenishmentTransaction(string name, decimal amount)
+    {
+        if (name == string.Empty)
+        {
+            throw new NotImplementedException();
+        }
+
+        Name = name;
+        Amount = amount;
+    }
+
+    public Guid Id { get; } = Guid.NewGuid();
+    public string Name { get; }
+    public decimal Amount { get; }
+
+    public decimal GetUndoResult(decimal accountMoney)
+    {
+        return accountMoney - Amount;
+    }
+}
---
+++ Lab4/Banks/Interfaces/ITransaction.cs
@@ -0,0 +1,9 @@
+﻿namespace Banks.Interfaces;
+
+public interface ITransaction
+{
+    public Guid Id { get; }
+    public string Name { get; }
+    public decimal Amount { get; }
+    public decimal GetUndoResult(decimal accountMoney);
+}
