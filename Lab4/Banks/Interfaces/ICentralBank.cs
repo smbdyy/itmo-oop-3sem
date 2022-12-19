998987f79802d4f73bf63ad337b59c22a5f52f4f
Ilya <ividivi67@gmail.com>
2022-12-19 13:15:01 +0300

    feat: make BankBuilder private with public setter and add CreateBank(string name) factory method

--- Lab4/Banks/Interfaces/ICentralBank.cs
+++ Lab4/Banks/Interfaces/ICentralBank.cs
@@ -6,9 +6,13 @@ namespace Banks.Interfaces;
 public interface ICentralBank
 {
     public DateOnly CurrentDate { get; }
-    public BankBuilder BankBuilder { get; }
+    public BankBuilder BankBuilder { set; }
 
-    public IBank CreateBank();
+    public void SetDefaultDepositAccountTerm(int term);
+    public void SetDefaultCreditAccountCommission(decimal commission);
+    public void SetDefaultCreditAccountLimit(decimal limit);
+    public void SetDefaultMaxUnverifiedClientWithdrawal(decimal value);
+    public IBank CreateBank(string name);
     public void RegisterClient(BankClient client);
     public void NotifyNextDay();
     public void DeleteClientAndAccounts(BankClient client);
