d2b1734bd7aa4a8774b55305f2192a631cd170f0
Ilya <ividivi67@gmail.com>
2022-12-19 12:42:02 +0300

    feat: add BankBuilder interface

---
+++ Lab4/Banks/Builders/IBankBuilder.cs
@@ -0,0 +1,14 @@
+﻿using Banks.Interfaces;
+
+namespace Banks.Builders;
+
+public interface IBankBuilder
+{
+    public IBank Build();
+    public void Reset();
+    public IBankBuilder SetName(string name);
+    public IBankBuilder SetDepositAccountTerm(int term);
+    public IBankBuilder SetCreditAccountCommission(decimal commission);
+    public IBankBuilder SetCreditAccountLimit(decimal limit);
+    public IBankBuilder SetMaxUnverifiedClientWithdrawal(decimal value);
+}
