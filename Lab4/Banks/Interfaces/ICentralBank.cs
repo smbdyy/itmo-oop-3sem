09c78a588999ac5a418f43d14d3d559ea0e0f3f0
Ilya <ividivi67@gmail.com>
2022-12-19 13:02:18 +0300

    feat: use builder instead of factory method

--- Lab4/Banks/Interfaces/ICentralBank.cs
+++ Lab4/Banks/Interfaces/ICentralBank.cs
@@ -1,17 +1,14 @@
-﻿using Banks.Entities;
+﻿using Banks.Builders;
+using Banks.Entities;
 
 namespace Banks.Interfaces;
 
 public interface ICentralBank
 {
     public DateOnly CurrentDate { get; }
+    public BankBuilder BankBuilder { get; }
 
-    public IBank CreateBank(
-        string name,
-        int depositAccountTerm,
-        decimal creditAccountCommission,
-        decimal creditAccountLimit,
-        decimal maxUnverifiedClientWithdrawal);
+    public IBank CreateBank();
     public void RegisterClient(BankClient client);
     public void NotifyNextDay();
     public void DeleteClientAndAccounts(BankClient client);
