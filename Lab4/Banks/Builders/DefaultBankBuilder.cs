780aed3bd6de463424e058520508915d1d7573bc
Ilya <ividivi67@gmail.com>
2022-12-19 13:01:02 +0300

    feat: add default bank builder

---
+++ Lab4/Banks/Builders/DefaultBankBuilder.cs
@@ -0,0 +1,22 @@
+﻿using Banks.Entities;
+using Banks.Interfaces;
+
+namespace Banks.Builders;
+
+public class DefaultBankBuilder : BankBuilder
+{
+    public override IBank Build()
+    {
+        if (Name is null)
+        {
+            throw new NotImplementedException();
+        }
+
+        return new Bank(
+            Name,
+            DepositAccountTerm,
+            CreditAccountCommission,
+            CreditAccountLimit,
+            MaxUnverifiedClientWithdrawal);
+    }
+}
