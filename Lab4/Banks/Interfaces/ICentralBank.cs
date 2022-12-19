2ad16381e92d8321011f38b9881abaae119c0ac5
Ilya <ividivi67@gmail.com>
2022-12-19 12:31:40 +0300

    feat: add ICentralBank interface

---
+++ Lab4/Banks/Interfaces/ICentralBank.cs
@@ -0,0 +1,18 @@
+﻿using Banks.Entities;
+
+namespace Banks.Interfaces;
+
+public interface ICentralBank
+{
+    public DateOnly CurrentDate { get; }
+
+    public IBank CreateBank(
+        string name,
+        int depositAccountTerm,
+        decimal creditAccountCommission,
+        decimal creditAccountLimit,
+        decimal maxUnverifiedClientWithdrawal);
+    public void RegisterClient(BankClient client);
+    public void NotifyNextDay();
+    public void DeleteClientAndAccounts(BankClient client);
+}
