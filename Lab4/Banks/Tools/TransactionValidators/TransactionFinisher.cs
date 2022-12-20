9aae1b63a2dd9baf5b5d1d6a459d700d727dc946
Ilya <ividivi67@gmail.com>
2022-12-21 01:23:43 +0300

    fix: '+' in replenishment

--- Lab4/Banks/Tools/TransactionValidators/TransactionFinisher.cs
+++ Lab4/Banks/Tools/TransactionValidators/TransactionFinisher.cs
@@ -12,7 +12,7 @@ public class TransactionFinisher : TransactionValidator
 
     public override decimal Replenish(IBankAccount account, decimal moneyAmount)
     {
-        return base.Replenish(account, moneyAmount) - moneyAmount;
+        return base.Replenish(account, moneyAmount) + moneyAmount;
     }
 
     public override decimal Send(TransferTransaction transaction)
