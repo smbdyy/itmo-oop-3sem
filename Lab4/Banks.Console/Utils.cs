37eec5a7048112b410dc08424e9622a06f79a8bc
Ilya <ividivi67@gmail.com>
2022-12-21 19:43:27 +0300

    feat: static class for working with console

---
+++ Lab4/Banks.Console/Utils.cs
@@ -0,0 +1,71 @@
+﻿namespace Banks.Console;
+
+public static class Utils
+{
+    public static int GetIntInput()
+    {
+        string? input = System.Console.ReadLine();
+        while (true)
+        {
+            try
+            {
+                return Convert.ToInt32(input);
+            }
+            catch
+            {
+                System.Console.WriteLine("incorrect input");
+                input = System.Console.ReadLine();
+            }
+        }
+    }
+
+    public static decimal GetDecimalInput()
+    {
+        string? input = System.Console.ReadLine();
+        while (true)
+        {
+            try
+            {
+                decimal value = Convert.ToDecimal(input);
+                if (value >= 0)
+                {
+                    return value;
+                }
+            }
+            catch
+            {
+                System.Console.WriteLine("incorrect input");
+            }
+        }
+    }
+
+    public static string GetStringInput()
+    {
+        string? input = System.Console.ReadLine();
+        while (input is null)
+        {
+            System.Console.WriteLine("incorrect input");
+            input = System.Console.ReadLine();
+        }
+
+        return input;
+    }
+
+    public static bool GetYesNoAnswerAsBool()
+    {
+        string input = GetStringInput();
+        while (true)
+        {
+            switch (input)
+            {
+                case "y":
+                    return true;
+                case "n":
+                    return false;
+                default:
+                    System.Console.WriteLine("incorrect input");
+                    break;
+            }
+        }
+    }
+}
