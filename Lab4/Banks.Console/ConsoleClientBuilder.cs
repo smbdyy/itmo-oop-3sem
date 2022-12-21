6ffcac1a927801060a36140ea25dbb05753e174e
Ilya <ividivi67@gmail.com>
2022-12-21 19:46:19 +0300

    feat: change exception message output

---
+++ Lab4/Banks.Console/ConsoleClientBuilder.cs
@@ -0,0 +1,75 @@
+﻿using Banks.Builders;
+using Banks.Interfaces;
+using Banks.Models;
+using ArgumentException = Banks.Tools.Exceptions.ArgumentException;
+
+namespace Banks.Console;
+
+public class ConsoleClientBuilder
+{
+    private ICentralBank _centralBank;
+    private BankClientBuilder _builder;
+
+    public ConsoleClientBuilder(ICentralBank centralBank, BankClientBuilder builder)
+    {
+        _centralBank = centralBank;
+        _builder = builder;
+    }
+
+    public void InputName()
+    {
+        while (true)
+        {
+            string value = Utils.GetStringInput();
+            try
+            {
+                _builder.SetName(PersonName.FromString(value));
+                return;
+            }
+            catch (ArgumentException ex)
+            {
+                System.Console.WriteLine($"exception: {ex.Message}");
+            }
+        }
+    }
+
+    public void InputAddress()
+    {
+        System.Console.WriteLine("do you want to input the address? (y/n)");
+        if (!Utils.GetYesNoAnswerAsBool()) return;
+
+        var builder = new ConsoleAddressBuilder(new AddressBuilder());
+        System.Console.WriteLine("country:");
+        builder.InputCountry();
+
+        System.Console.WriteLine("town:");
+        builder.InputCountry();
+
+        System.Console.WriteLine("street:");
+        builder.InputStreet();
+
+        System.Console.WriteLine("house number");
+        builder.InputHouseNumber();
+
+        _builder.SetAddress(builder.Build());
+    }
+
+    public void InputPassportNumber()
+    {
+        System.Console.WriteLine("do you want to input the password number? (y/n)");
+        if (!Utils.GetYesNoAnswerAsBool()) return;
+
+        while (true)
+        {
+            try
+            {
+                _builder.SetPassportNumber(PassportNumber.FromString(Utils.GetStringInput()));
+                return;
+            }
+            catch (ArgumentException ex)
+            {
+                System.Console.WriteLine($"exception: {ex.Message}");
+            }
+        }
+    }
+}
