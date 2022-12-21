b4d0acf5aaf46a95a1e17ece4d798636f3d45c25
Ilya <ividivi67@gmail.com>
2022-12-21 19:44:10 +0300

    feat: console address builder

---
+++ Lab4/Banks.Console/ConsoleAddressBuilder.cs
@@ -0,0 +1,84 @@
+﻿using Banks.Builders;
+using Banks.Models;
+using ArgumentException = Banks.Tools.Exceptions.ArgumentException;
+
+namespace Banks.Console;
+
+public class ConsoleAddressBuilder
+{
+    private AddressBuilder _builder;
+
+    public ConsoleAddressBuilder(AddressBuilder builder)
+    {
+        _builder = builder;
+    }
+
+    public void InputCountry()
+    {
+        while (true)
+        {
+            try
+            {
+                _builder.SetCountry(Utils.GetStringInput());
+                return;
+            }
+            catch (ArgumentException ex)
+            {
+                System.Console.WriteLine(ex.Message);
+            }
+        }
+    }
+
+    public void InputTown()
+    {
+        while (true)
+        {
+            try
+            {
+                _builder.SetTown(Utils.GetStringInput());
+                return;
+            }
+            catch (ArgumentException ex)
+            {
+                System.Console.WriteLine(ex.Message);
+            }
+        }
+    }
+
+    public void InputStreet()
+    {
+        while (true)
+        {
+            try
+            {
+                _builder.SetStreet(Utils.GetStringInput());
+                return;
+            }
+            catch (ArgumentException ex)
+            {
+                System.Console.WriteLine(ex.Message);
+            }
+        }
+    }
+
+    public void InputHouseNumber()
+    {
+        while (true)
+        {
+            try
+            {
+                _builder.SetTown(Utils.GetStringInput());
+                return;
+            }
+            catch (ArgumentException ex)
+            {
+                System.Console.WriteLine(ex.Message);
+            }
+        }
+    }
+
+    public Address Build()
+    {
+        return _builder.Build();
+    }
+}
