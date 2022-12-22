c5e43a25b5ad2b90805e8ba2f15c8daacfb87dd3
Ilya <ividivi67@gmail.com>
2022-12-22 16:22:43 +0300

    feat: add commands set_term, set_cred_c, set_cred_l, set_unv_l

--- Lab4/Banks.Console/BankConsoleInterface.cs
+++ Lab4/Banks.Console/BankConsoleInterface.cs
@@ -1,4 +1,5 @@
 ﻿using Banks.Interfaces;
+using ArgumentException = Banks.Tools.Exceptions.ArgumentException;
 
 namespace Banks.Console;
 
@@ -40,6 +41,22 @@ public class BankConsoleInterface
                     _centralBank.DeleteBank(_bank);
                     System.Console.WriteLine("bank has been deleted, returning to main menu");
                     return;
+                case "set_term":
+                    SetDepositAccountTerm();
+                    System.Console.WriteLine("deposit account term has been set");
+                    break;
+                case "set_cred_c":
+                    SetCreditAccountCommission();
+                    System.Console.WriteLine("credit account commission has been set");
+                    break;
+                case "set_cred_l":
+                    SetCreditAccountLimit();
+                    System.Console.WriteLine("credit account limit has been set");
+                    break;
+                case "set_unv_l":
+                    SetUnverifiedClientWithdrawalLimit();
+                    System.Console.WriteLine("unverified client withdrawal limit has been set");
+                    break;
                 default:
                     System.Console.WriteLine("incorrect input");
                     break;
@@ -56,5 +73,65 @@ public class BankConsoleInterface
         System.Console.WriteLine($"Credit commission: {_bank.CreditAccountCommission}");
         System.Console.WriteLine($"Credit account limit: {_bank.CreditAccountLimit}");
         System.Console.WriteLine($"Unverified client withdrawal limit: {_bank.UnverifiedClientWithdrawalLimit}");
+    }
+
+    private void SetDepositAccountTerm()
+    {
+        while (true)
+        {
+            try
+            {
+                _bank.DepositAccountTerm = Utils.GetIntInput();
+            }
+            catch (ArgumentException ex)
+            {
+                System.Console.WriteLine($"exception: {ex.Message}");
+            }
+        }
+    }
+
+    private void SetCreditAccountCommission()
+    {
+        while (true)
+        {
+            try
+            {
+                _bank.CreditAccountCommission = Utils.GetDecimalInput();
+            }
+            catch (ArgumentException ex)
+            {
+                System.Console.WriteLine($"exception: {ex.Message}");
+            }
+        }
+    }
+
+    private void SetCreditAccountLimit()
+    {
+        while (true)
+        {
+            try
+            {
+                _bank.CreditAccountLimit = Utils.GetDecimalInput();
+            }
+            catch (ArgumentException ex)
+            {
+                System.Console.WriteLine($"exception: {ex.Message}");
+            }
+        }
+    }
+
+    private void SetUnverifiedClientWithdrawalLimit()
+    {
+        while (true)
+        {
+            try
+            {
+                _bank.UnverifiedClientWithdrawalLimit = Utils.GetDecimalInput();
+            }
+            catch (ArgumentException ex)
+            {
+                System.Console.WriteLine($"exception: {ex.Message}");
+            }
+        }
     }
 }
