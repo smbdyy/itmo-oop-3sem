f40b38f8d3bae87ad4eca4c0608b4d42e1e872d2
Ilya <ividivi67@gmail.com>
2022-12-22 15:32:00 +0300

    feat: add main console interface and remake Program.cs using it

---
+++ Lab4/Banks.Console/MainConsoleInterface.cs
@@ -0,0 +1,105 @@
+﻿using Banks.Builders;
+using Banks.Entities;
+using Banks.Interfaces;
+
+namespace Banks.Console;
+
+public class MainConsoleInterface
+{
+    private ICentralBank _centralBank;
+    private CentralBankConsoleInterface _centralBankInterface;
+
+    public MainConsoleInterface(ICentralBank centralBank)
+    {
+        _centralBank = centralBank;
+        _centralBankInterface = new CentralBankConsoleInterface(_centralBank);
+    }
+
+    public void Start()
+    {
+        System.Console.WriteLine(
+            @"initialized, commands:
+                exit
+                create_b - create new bank
+                select_b - select bank to manage
+                create_c - create client
+                select_c - select client to manage
+        ");
+
+        while (true)
+        {
+            string input = Utils.GetStringInput();
+            switch (input)
+            {
+                case "exit":
+                    return;
+                case "create_b":
+                    CreateBank();
+                    break;
+                case "create_c":
+                    CreateClient();
+                    break;
+                case "select_c":
+                    SelectClient();
+                    break;
+                default:
+                    System.Console.WriteLine("incorrect input");
+                    break;
+            }
+        }
+    }
+
+    private void CreateBank()
+    {
+        _centralBankInterface.InputAllBankData();
+        IBank bank = _centralBankInterface.InputNameCreateBank();
+        System.Console.WriteLine($"bank created, id: {bank.Id}");
+    }
+
+    private void CreateClient()
+    {
+        var builder = new ConsoleClientBuilder(_centralBank, new BankClientBuilder());
+        builder.InputAllData();
+        BankClient client = builder.Build();
+        System.Console.WriteLine($"client created, id: {client.Id}");
+    }
+
+    private void SelectClient()
+    {
+        if (_centralBank.Clients.Count == 0)
+        {
+            System.Console.WriteLine("no clients found");
+            return;
+        }
+
+        WriteClientsList();
+        BankClient selectedClient = GetClientByInputNumber();
+        var clientInterface = new ClientConsoleInterface(_centralBank, selectedClient);
+        clientInterface.Start();
+    }
+
+    private void WriteClientsList()
+    {
+        var clients = _centralBank.Clients.ToList();
+        for (int i = 0; i < clients.Count; i++)
+        {
+            System.Console.WriteLine($"{i}. {clients[i].Name.AsString}, id {clients[i].Id}");
+        }
+    }
+
+    private BankClient GetClientByInputNumber()
+    {
+        var clients = _centralBank.Clients.ToList();
+        System.Console.WriteLine("input a number:");
+        while (true)
+        {
+            int number = Utils.GetIntInput();
+            if (number >= 0 && number < clients.Count)
+            {
+                return clients[number];
+            }
+
+            System.Console.WriteLine("incorrect input");
+        }
+    }
+}
--- Lab4/Banks.Console/Program.cs
+++ Lab4/Banks.Console/Program.cs
@@ -1,42 +1,7 @@
 ﻿using Banks.Builders;
 using Banks.Console;
 using Banks.Entities;
-using Banks.Interfaces;
 
 var centralBank = new CentralBank(new DefaultBankBuilder());
-var centralBankInterface = new CentralBankConsoleInterface(centralBank);
-
-Console.WriteLine(
-    @"initialized, commands:
-    exit
-    create_b - create new bank
-    select_b - select bank to manage
-    create_c - create client
-    select_c - select client to manage
-");
-
-while (true)
-{
-    string input = Utils.GetStringInput();
-    switch (input)
-    {
-        case "exit":
-            return;
-        case "create_b":
-            centralBankInterface.InputAllData();
-            IBank bank = centralBankInterface.InputNameCreateBank();
-            Console.WriteLine($"bank created, id: {bank.Id}");
-            break;
-
-        case "create_c":
-            var builder = new ConsoleClientBuilder(centralBank, new BankClientBuilder());
-            builder.InputAllData();
-            BankClient client = builder.Build();
-            Console.WriteLine($"client created, id: {client.Id}");
-            break;
-
-        default:
-            Console.WriteLine("incorrect input");
-            break;
-    }
-}
+var mainInterface = new MainConsoleInterface(centralBank);
+mainInterface.Start();
