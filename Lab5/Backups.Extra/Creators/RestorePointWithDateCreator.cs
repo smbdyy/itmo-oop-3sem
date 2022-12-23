8c3d52e2acff341d2488417d1e99050ea8b33dbd
Ilya <ividivi67@gmail.com>
2022-12-23 17:46:14 +0300

    feat: add restore point with custom date creation stuff

---
+++ Lab5/Backups.Extra/Creators/RestorePointWithDateCreator.cs
@@ -0,0 +1,21 @@
+﻿using Backups.Extra.Models;
+using Backups.Models;
+using Backups.StorageAlgorithms;
+using Backups.Tools.Creators;
+
+namespace Backups.Extra.Creators;
+
+public class RestorePointWithDateCreator : IRestorePointCreator
+{
+    private DateTime _dateTime = DateTime.Now;
+
+    public void SetDateTime(DateTime dateTime)
+    {
+        _dateTime = dateTime;
+    }
+
+    public IRestorePoint Create(IEnumerable<IBackupObject> backupObjects, IStorage storage)
+    {
+        return new RestorePointWithDate(backupObjects, storage, _dateTime);
+    }
+}
---
+++ Lab5/Backups.Extra/Models/RestorePointWithDate.cs
@@ -0,0 +1,20 @@
+﻿using Backups.Models;
+using Backups.StorageAlgorithms;
+
+namespace Backups.Extra.Models;
+
+public class RestorePointWithDate : IRestorePoint
+{
+    private List<IBackupObject> _backupObjects;
+
+    public RestorePointWithDate(IEnumerable<IBackupObject> backupObjects, IStorage storage, DateTime creationDateTime)
+    {
+        _backupObjects = backupObjects.ToList();
+        Storage = storage;
+        CreationDateTime = creationDateTime;
+    }
+
+    public IReadOnlyCollection<IBackupObject> BackupObjects => _backupObjects;
+    public DateTime CreationDateTime { get; }
+    public IStorage Storage { get; }
+}
