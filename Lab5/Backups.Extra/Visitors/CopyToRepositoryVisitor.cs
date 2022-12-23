3e9b172de663724b523dfa0b193ae83baa9f4dfd
Ilya <ividivi67@gmail.com>
2022-12-23 20:51:06 +0300

    feat: add CopyToRepositoryVisitor

---
+++ Lab5/Backups.Extra/Visitors/CopyToRepositoryVisitor.cs
@@ -0,0 +1,37 @@
+﻿using Backups.Repositories;
+using Backups.Visitors;
+
+namespace Backups.Extra.Visitors;
+
+public class CopyToRepositoryVisitor : IRepositoryVisitor
+{
+    private readonly IRepository _repository;
+
+    public CopyToRepositoryVisitor(IRepository repository)
+    {
+        _repository = repository;
+    }
+
+    public void Visit(IRepositoryFolder repositoryFolder)
+    {
+        if (!_repository.DirectoryExists(repositoryFolder.Path))
+        {
+            _repository.CreateDirectory(repositoryFolder.Path);
+        }
+
+        foreach (IRepositoryObject repositoryObject in repositoryFolder.Entries)
+        {
+            repositoryObject.Accept(this);
+        }
+    }
+
+    public void Visit(IRepositoryFile repositoryFile)
+    {
+        if (_repository.FileExists(repositoryFile.Path)) return;
+
+        _repository.CreateFile(repositoryFile.Path);
+        using Stream createdFileStream = _repository.OpenWrite(repositoryFile.Path);
+        using Stream repositoryFileStream = repositoryFile.Open();
+        repositoryFileStream.CopyTo(createdFileStream);
+    }
+}
