ef277219038d813c8f06e5d44758589ace69b694
Ilya <ividivi67@gmail.com>
2022-12-24 16:28:25 +0300

    feat: add user interaction interfaces

---
+++ Lab4/Banks.Console/UserInteractionInterfaces/ConsoleUserInteractionInterface.cs
@@ -0,0 +1,9 @@
+﻿namespace Banks.Console.UserInteractionInterfaces;
+
+public class ConsoleUserInteractionInterface : IUserInteractionInterface
+{
+    public int Read() => System.Console.Read();
+    public string? ReadLine() => System.Console.ReadLine();
+    public void Write(string value) => System.Console.Write(value);
+    public void WriteLine(string value) => System.Console.WriteLine(value);
+}
---
+++ Lab4/Banks.Console/UserInteractionInterfaces/IUserInteractionInterface.cs
@@ -0,0 +1,9 @@
+﻿namespace Banks.Console.UserInteractionInterfaces;
+
+public interface IUserInteractionInterface
+{
+    public int Read();
+    public string? ReadLine();
+    public void Write(string value);
+    public void WriteLine(string value);
+}
