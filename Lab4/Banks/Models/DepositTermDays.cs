0cbddb596bb4b463cdb0420fe4fe790c9aec5461
Ilya <ividivi67@gmail.com>
2022-12-24 12:58:53 +0300

    feat: add numeric value objects

---
+++ Lab4/Banks/Models/DepositTermDays.cs
@@ -0,0 +1,21 @@
+﻿using ArgumentException = Banks.Tools.Exceptions.ArgumentException;
+
+namespace Banks.Models;
+
+public struct DepositTermDays
+{
+    public DepositTermDays(int value)
+    {
+        if (value < 0)
+        {
+            throw ArgumentException.InappropriateNegativeNumber(value);
+        }
+
+        Value = value;
+    }
+
+    public int Value { get; }
+
+    public static implicit operator int(DepositTermDays value) => value.Value;
+    public static implicit operator DepositTermDays(int value) => new DepositTermDays(value);
+}
---
+++ Lab4/Banks/Models/NonPositiveMoneyAmount.cs
@@ -0,0 +1,14 @@
+﻿namespace Banks.Models;
+
+public struct NonPositiveMoneyAmount
+{
+    public NonPositiveMoneyAmount(decimal value)
+    {
+        Value = value;
+    }
+
+    public decimal Value { get; }
+
+    public static implicit operator decimal(NonPositiveMoneyAmount value) => value.Value;
+    public static implicit operator NonPositiveMoneyAmount(decimal value) => new NonPositiveMoneyAmount(value);
+}
