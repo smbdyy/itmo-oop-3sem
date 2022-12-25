a267fa06bfdf2ae3fb58dbe698606a10e5b13f51
Ilya <ividivi67@gmail.com>
2022-12-26 01:58:00 +0300

    feat: add days command handler

---
+++ Lab4/Banks.Console/MainMenuCommandHandlers/AddDaysCommandHandler.cs
@@ -0,0 +1,30 @@
+﻿using System.Text.RegularExpressions;
+using Banks.Console.UserInteractionInterfaces;
+using Banks.Interfaces;
+
+namespace Banks.Console.MainMenuCommandHandlers;
+
+public class AddDaysCommandHandler : MainMenuCommandHandler
+{
+    private readonly ICentralBank _centralBank;
+
+    public AddDaysCommandHandler(ICentralBank centralBank, IUserInteractionInterface interactionInterface)
+        : base(interactionInterface) => _centralBank = centralBank;
+
+    public override void Handle(string command)
+    {
+        if (Regex.IsMatch(command, "^add_days\\s[1-9][0-9]*$"))
+        {
+            int number = Convert.ToInt32(command.Split(' ')[1]);
+            for (int i = 0; i < number; i++)
+            {
+                _centralBank.NotifyNextDay();
+            }
+
+            InteractionInterface.WriteLine($"success, current date: {_centralBank.CurrentDate.ToString()}");
+            return;
+        }
+
+        base.Handle(command);
+    }
+}
