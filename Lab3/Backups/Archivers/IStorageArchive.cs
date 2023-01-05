5dfb36787b3ced32b9b98e26e03d821ffd4eb85c
Ilya <ividivi67@gmail.com>
2023-01-06 01:33:52 +0500

    chore: delete useless stuff

--- Lab3/Backups/Archivers/IStorageArchive.cs
+++
@@ -1,9 +0,0 @@
-﻿using Backups.Repositories;
-
-namespace Backups.Archivers;
-
-public interface IStorageArchive
-{
-    public string Name { get; }
-    public IReadOnlyCollection<IRepositoryObject> GetEntries();
-}
