8d18e2ffc2a40e658bdd4083da6d0f361a225ef4
Ilya <ividivi67@gmail.com>
2023-01-06 01:38:23 +0500

    feat: add new Storage class instead of SingleStorage and SplitStorage

---
+++ Lab3/Backups/StorageAlgorithms/Storage.cs
@@ -0,0 +1,16 @@
+﻿using Backups.Archivers;
+using Backups.Repositories;
+
+namespace Backups.StorageAlgorithms;
+
+public class Storage : IStorage
+{
+    private readonly List<IRepositoryObject> _entries;
+
+    public Storage(IEnumerable<IRepositoryObject> entries)
+    {
+        _entries = entries.ToList();
+    }
+
+    public IReadOnlyCollection<IRepositoryObject> GetEntries() => _entries;
+}
