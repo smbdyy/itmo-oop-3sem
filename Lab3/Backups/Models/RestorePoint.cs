927d868fd0bb64d07d7abe3383a213d3c0d4173e
Ilya <ividivi67@gmail.com>
2023-01-12 15:39:04 +0300

    feat: add FolderName property fix: add readonly modifier to _backupObjects field

--- Lab3/Backups/Models/RestorePoint.cs
+++ Lab3/Backups/Models/RestorePoint.cs
@@ -4,14 +4,21 @@ namespace Backups.Models;
 
 public class RestorePoint : IRestorePoint
 {
-    private List<IBackupObject> _backupObjects;
+    private readonly List<IBackupObject> _backupObjects;
 
-    public RestorePoint(IEnumerable<IBackupObject> backupObjects, IStorage storage)
+    public RestorePoint(string folderName, IEnumerable<IBackupObject> backupObjects, IStorage storage)
     {
+        if (folderName == string.Empty)
+        {
+            throw new NotImplementedException();
+        }
+
+        FolderName = folderName;
         _backupObjects = backupObjects.ToList();
         Storage = storage;
     }
 
+    public string FolderName { get; }
     public IReadOnlyCollection<IBackupObject> BackupObjects => _backupObjects;
     public DateTime CreationDateTime { get; } = DateTime.Now;
     public IStorage Storage { get; }
