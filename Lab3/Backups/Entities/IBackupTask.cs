0888a84ae0fae9fd9e9586dfde5277bdffc1e555
Ilya <ividivi67@gmail.com>
2023-01-12 15:52:17 +0300

    feat: remove DeleteRestorePoint(), add restore point folder creation in CreateRestorePoint(), add ArchiveExtensionProperty

--- Lab3/Backups/Entities/IBackupTask.cs
+++ Lab3/Backups/Entities/IBackupTask.cs
@@ -6,11 +6,11 @@ namespace Backups.Entities;
 public interface IBackupTask
 {
     public string Name { get; }
+    public string ArchiveExtension { get; }
     public IRepository Repository { get; }
     public IReadOnlyCollection<IRestorePoint> RestorePoints { get; }
     public IReadOnlyCollection<IBackupObject> BackupObjects { get; }
     public void CreateRestorePoint();
-    public void DeleteRestorePoint(IRestorePoint restorePoint);
     public void AddBackupObject(IBackupObject backupObject);
     public void RemoveBackupObject(IBackupObject backupObject);
 }
