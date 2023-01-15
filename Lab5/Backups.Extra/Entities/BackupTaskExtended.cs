9b2e353c0e3443255f0a0e381c3ff29b20338021
Ilya <ividivi67@gmail.com>
2023-01-15 20:06:23 +0300

    feat: implement Add/RemoveBackupObject and CleanupRestorePoints methods

--- Lab5/Backups.Extra/Entities/BackupTaskExtended.cs
+++ Lab5/Backups.Extra/Entities/BackupTaskExtended.cs
@@ -5,6 +5,7 @@ using Backups.Models;
 using Backups.Repositories;
 using Backups.StorageAlgorithms;
 using Backups.Tools.Creators;
+using Backups.Tools.Exceptions;
 
 namespace Backups.Extra.Entities;
 
@@ -17,12 +18,14 @@ public class BackupTaskExtended : IBackupTask
     private readonly IStorageAlgorithm _storageAlgorithm;
     private readonly IStorageArchiver _archiver;
     private readonly IRestorePointCreator _restorePointCreator;
+    private readonly IRestorePointsCleaner _cleaner;
     private int _newRestorePointId;
 
     public BackupTaskExtended(
         string name,
         IRepository repository,
         IRestorePointDeleteSelector deleteSelector,
+        IRestorePointsCleaner cleaner,
         IBackupTaskLogger logger,
         IStorageAlgorithm storageAlgorithm,
         IStorageArchiver archiver,
@@ -35,6 +38,7 @@ public class BackupTaskExtended : IBackupTask
         _storageAlgorithm = storageAlgorithm;
         _archiver = archiver;
         _restorePointCreator = restorePointCreator;
+        _cleaner = cleaner;
     }
 
     public string Name { get; }
@@ -43,8 +47,32 @@ public class BackupTaskExtended : IBackupTask
     public IReadOnlyCollection<IRestorePoint> RestorePoints => _restorePoints;
     public IReadOnlyCollection<IBackupObject> BackupObjects => _backupObjects;
 
-    public void AddBackupObject(IBackupObject backupObject) { }
-    public void RemoveBackupObject(IBackupObject backupObject) { }
+    public void AddBackupObject(IBackupObject backupObject)
+    {
+        if (_backupObjects.Any(obj => obj.Path == backupObject.Path))
+        {
+            throw BackupTaskException.AlreadyTracking(backupObject, this);
+        }
+
+        Repository.ValidateRelativePath(backupObject.Path);
+        if (!Repository.DirectoryExists(backupObject.Path) && !Repository.FileExists(backupObject.Path))
+        {
+            throw BackupTaskException.NotFoundInRepository(backupObject, this);
+        }
+
+        _backupObjects.Add(backupObject);
+    }
+
+    public void RemoveBackupObject(IBackupObject backupObject)
+    {
+        if (!_backupObjects.Contains(backupObject))
+        {
+            throw BackupTaskException.NotTracking(backupObject, this);
+        }
+
+        _backupObjects.Remove(backupObject);
+    }
+
     public void CreateRestorePoint() { }
 
     private void DeleteRestorePoint(IRestorePoint restorePoint)
@@ -56,6 +84,45 @@ public class BackupTaskExtended : IBackupTask
 
         Repository.DeleteDirectory(Path.Combine(Repository.RestorePointsPath, restorePoint.FolderName));
         _restorePoints.Remove(restorePoint);
+    }
+
+    private void CleanupRestorePoints()
+    {
+        var pointsToDelete = _deleteSelector.GetRestorePointsToDelete(RestorePoints).ToList();
+        if (pointsToDelete.Count == 0)
+        {
+            return;
+        }
+
+        if (_storageAlgorithm is SingleStorageAlgorithm || pointsToDelete.Count == RestorePoints.Count)
+        {
+            foreach (IRestorePoint point in pointsToDelete)
+            {
+                DeleteRestorePoint(point);
+            }
+
+            return;
+        }
+
+        IEnumerable<IRepositoryObject> newPointRepositoryObjects =
+            _cleaner.GetNewPointRepositoryObjects(pointsToDelete, _restorePoints.Last());
+        _restorePoints.Add(CreateRestorePointFromRepositoryObjects(newPointRepositoryObjects));
+        foreach (IRestorePoint point in pointsToDelete)
+        {
+            DeleteRestorePoint(point);
+        }
+    }
+
+    private IRestorePoint CreateRestorePointFromRepositoryObjects(
+        IEnumerable<IRepositoryObject> repositoryObjects)
+    {
+        int id = GetNewRestorePointId();
+        string folderName = $"RestorePoint_{id}";
+        string path = Path.Combine(Repository.RestorePointsPath, folderName);
+        Repository.CreateDirectory(path);
+
+        IStorage storage = _storageAlgorithm.MakeStorage(id, path, Repository, _archiver, repositoryObjects);
+        return _restorePointCreator.Create(folderName, _backupObjects, storage);
     }
 
     private int GetNewRestorePointId() => _newRestorePointId++;
