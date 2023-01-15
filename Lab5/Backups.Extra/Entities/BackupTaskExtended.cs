9663ab07c894951b4932f193c0c798a6f6447505
Ilya <ividivi67@gmail.com>
2023-01-15 15:02:36 +0300

    feat: add basic private fields and ctor

--- Lab5/Backups.Extra/Entities/BackupTaskExtended.cs
+++ Lab5/Backups.Extra/Entities/BackupTaskExtended.cs
@@ -1,8 +1,34 @@
-﻿using Backups.Entities;
+﻿using Backups.Archivers;
+using Backups.Entities;
+using Backups.Extra.Interfaces;
+using Backups.Models;
+using Backups.StorageAlgorithms;
+using Backups.Tools.Creators;
 
 namespace Backups.Extra.Entities;
 
 public class BackupTaskExtended : IBackupTask
 {
+    private readonly IRestorePointDeleteSelector _deleteSelector;
+    private readonly IBackupTaskLogger _logger;
+    private readonly List<IRestorePoint> _restorePoints = new ();
+    private readonly List<IBackupObject> _backupObjects = new ();
+    private readonly IStorageAlgorithm _storageAlgorithm;
+    private readonly IStorageArchiver _archiver;
+    private readonly IRestorePointCreator _restorePointCreator;
+    private int _newRestorePointId;
 
+    public BackupTaskExtended(
+        IRestorePointDeleteSelector deleteSelector,
+        IBackupTaskLogger logger,
+        IStorageAlgorithm storageAlgorithm,
+        IStorageArchiver archiver,
+        IRestorePointCreator restorePointCreator)
+    {
+        _deleteSelector = deleteSelector;
+        _logger = logger;
+        _storageAlgorithm = storageAlgorithm;
+        _archiver = archiver;
+        _restorePointCreator = restorePointCreator;
+    }
 }
