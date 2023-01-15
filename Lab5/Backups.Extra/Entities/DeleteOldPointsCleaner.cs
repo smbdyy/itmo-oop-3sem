7bcda7b5b7d1f3870672ecbeb7bb0d0ebac12fb0
Ilya <ividivi67@gmail.com>
2023-01-15 20:04:14 +0300

    feat: add restore point cleaners

---
+++ Lab5/Backups.Extra/Entities/DeleteOldPointsCleaner.cs
@@ -0,0 +1,14 @@
+﻿using Backups.Extra.Interfaces;
+using Backups.Models;
+using Backups.Repositories;
+
+namespace Backups.Extra.Entities;
+
+public class DeleteOldPointsCleaner : IRestorePointsCleaner
+{
+    public IEnumerable<IRepositoryObject> GetNewPointRepositoryObjects(
+        IEnumerable<IRestorePoint> pointToDelete, IRestorePoint lastPoint)
+    {
+        return lastPoint.Storage.GetEntries();
+    }
+}
---
+++ Lab5/Backups.Extra/Entities/MergePointsCleaner.cs
@@ -0,0 +1,30 @@
+﻿using Backups.Extra.Interfaces;
+using Backups.Models;
+using Backups.Repositories;
+
+namespace Backups.Extra.Entities;
+
+public class MergePointsCleaner : IRestorePointsCleaner
+{
+    public IEnumerable<IRepositoryObject> GetNewPointRepositoryObjects(
+        IEnumerable<IRestorePoint> pointToDelete, IRestorePoint lastPoint)
+    {
+        var newPointRepositoryObjects = lastPoint.Storage.GetEntries().ToList();
+        var pointsToDeleteAsList = pointToDelete.ToList();
+        pointsToDeleteAsList.Reverse();
+
+        foreach (IRestorePoint point in pointsToDeleteAsList)
+        {
+            IEnumerable<IRepositoryObject> pointStorageEntries = point.Storage.GetEntries();
+            foreach (IRepositoryObject storageEntry in pointStorageEntries)
+            {
+                if (newPointRepositoryObjects.All(o => o.Path != storageEntry.Path))
+                {
+                    newPointRepositoryObjects.Add(storageEntry);
+                }
+            }
+        }
+
+        return newPointRepositoryObjects;
+    }
+}
