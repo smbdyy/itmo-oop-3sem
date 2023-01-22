268df9926800a49331b25edfbfd9abfd694196e9
Ilya <ividivi67@gmail.com>
2023-01-22 20:58:34 +0300

    feat: add RestorePointMessageGenerator

---
+++ Lab5/Backups.Extra/LoggerMessageGenerators/RestorePointMessageGenerator.cs
@@ -0,0 +1,22 @@
+﻿using Backups.Extra.Interfaces;
+using Backups.Models;
+
+namespace Backups.Extra.LoggerMessageGenerators;
+
+public class RestorePointMessageGenerator : ILoggerMessageGenerator
+{
+    private readonly IRestorePoint _restorePoint;
+
+    public RestorePointMessageGenerator(IRestorePoint restorePoint)
+    {
+        _restorePoint = restorePoint;
+    }
+
+    public string GetMessage()
+    {
+        string message =
+            $"restore point (creation time: {_restorePoint.CreationDateTime}, folder: {_restorePoint.FolderName}, objects:";
+        return _restorePoint.BackupObjects
+            .Aggregate(message, (current, backupObject) => current + Environment.NewLine + backupObject.Path);
+    }
+}
