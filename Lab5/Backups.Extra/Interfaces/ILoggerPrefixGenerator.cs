42879d25f1bcec4bdb7c179471c38d9aa6463280
Ilya <ividivi67@gmail.com>
2023-01-22 16:06:17 +0300

    feat: add logger message prefix generators

---
+++ Lab5/Backups.Extra/Interfaces/ILoggerPrefixGenerator.cs
@@ -0,0 +1,6 @@
+﻿namespace Backups.Extra.Interfaces;
+
+public interface ILoggerPrefixGenerator
+{
+    public string GetPrefix();
+}
---
+++ Lab5/Backups.Extra/LoggerMessageGenerators/EmptyPrefixGenerator.cs
@@ -0,0 +1,8 @@
+﻿using Backups.Extra.Interfaces;
+
+namespace Backups.Extra.LoggerMessageGenerators;
+
+public class EmptyPrefixGenerator : ILoggerPrefixGenerator
+{
+    public string GetPrefix() => string.Empty;
+}
