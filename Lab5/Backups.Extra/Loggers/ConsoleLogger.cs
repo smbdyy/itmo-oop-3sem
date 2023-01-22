a21887041eea319352c1a14f5b6387587f6298c2
Ilya <ividivi67@gmail.com>
2023-01-22 17:59:23 +0300

    feat: add loggers

---
+++ Lab5/Backups.Extra/Loggers/ConsoleLogger.cs
@@ -0,0 +1,18 @@
+﻿using Backups.Extra.Interfaces;
+
+namespace Backups.Extra.Loggers;
+
+public class ConsoleLogger : IBackupTaskLogger
+{
+    private readonly ILoggerPrefixGenerator _prefixGenerator;
+
+    public ConsoleLogger(ILoggerPrefixGenerator prefixGenerator)
+    {
+        _prefixGenerator = prefixGenerator;
+    }
+
+    public void WriteLog(string eventMessage, ILoggerMessageGenerator entityMessageGenerator)
+    {
+        Console.WriteLine($"{_prefixGenerator.GetPrefix()} {eventMessage} {entityMessageGenerator.GetMessage()}");
+    }
+}
---
+++ Lab5/Backups.Extra/Loggers/FileLogger.cs
@@ -0,0 +1,26 @@
+﻿using Backups.Extra.Interfaces;
+
+namespace Backups.Extra.Loggers;
+
+public class FileLogger : IBackupTaskLogger
+{
+    private readonly ILoggerPrefixGenerator _prefixGenerator;
+    private readonly string _filePath;
+
+    public FileLogger(ILoggerPrefixGenerator prefixGenerator, string filePath)
+    {
+        if (!File.Exists(filePath))
+        {
+            throw new NotImplementedException();
+        }
+
+        _prefixGenerator = prefixGenerator;
+        _filePath = filePath;
+    }
+
+    public void WriteLog(string eventMessage, ILoggerMessageGenerator entityMessageGenerator)
+    {
+        using var writer = new StreamWriter(_filePath);
+        writer.WriteLine($"{_prefixGenerator.GetPrefix()} {eventMessage} {entityMessageGenerator.GetMessage()}");
+    }
+}
